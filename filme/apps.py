23397bc15c6a9395a45002120170b7fd567ca0cf
Ismael <ismaelsleifer@gmail.com>
2024-10-29 21:48:31 -0300

    adicionado o create do super user

--- filme/apps.py
+++ filme/apps.py
@@ -16,5 +16,3 @@ class FilmeConfig(AppConfig):
 
         if not usuarios:
             Usuario.objects.create_superuser(username='admin', email=email, password=senha, is_active=True, is_staff=True)
-
-        return super().ready()
